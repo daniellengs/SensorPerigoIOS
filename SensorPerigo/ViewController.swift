264be489e66110e2cb7e27cd5ff593ba650d6e6f
erikapessoa <erika.pessoa@gmail.com>
2018-05-12 09:42:02 -0300

    Initial Commit

---
+++ SensorPerigo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SensorPerigo
+//
+//  Created by Danielle Gomes on 12/05/2018.
+//  Copyright © 2018 CESAR School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
